15fe97131eb17f287730f5ea539b77e2dcd07c2d
@flinch04 <81803942+wanjohy@users.noreply.github.com>
2024-03-15 17:11:25 +0300

    Update settings.py

--- chemist_backend/settings.py
+++ chemist_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%c_jd)fn3yvb$$^tp731&$6v*pyrv(x!psyo^_sn7v$7)!d@54
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = []
 
 
